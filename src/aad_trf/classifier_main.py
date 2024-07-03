9b5431fe2d857cedd6d2feb25b65cd3b9b388f19
Jusung Ham <jusungham25@gmail.com>
2024-07-03 12:46:39 -0500

    feat: updated classification process accordingly
    - Updated configuration loading and file naming in `classifier_main.py`.
    - Made a new json config file for a classifier process.

--- src/aad_trf/classifier_main.py
+++ src/aad_trf/classifier_main.py
@@ -12,19 +12,22 @@ from aad_plotter import plot_clf_results
 from utils import load_config, set_paths_from_config, get_field_from_filename
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config_id", type=str, default="dataset-updown-nh_exp-1", help="Configuration ID")
+parser.add_argument("--config_id", type=str, default="exp-001", help="Configuration ID")
 args = parser.parse_args()
 config_id = args.config_id
-
-base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 config = load_config(config_id)
-config_trf = config['trf']
+config_id_trf = config['trf']['config_id']
+config_trf = load_config(config_id_trf)
+config_id_clf = config['classifier']['config_id']
+config_clf = load_config(config_id_clf)
+
+dataset_name = config['dataset']
+base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 path_dict = set_paths_from_config(base_path, config)
-config_clf = config['classifiers']
 
-files = glob.glob(f"{path_dict['features']}/{config_id}_data-aad-trfscores*.pkl")
+files = glob.glob(f"{path_dict['features']}/dataset-{dataset_name}_data-aad-trfscores_config-{config_id_trf}_sub-*.pkl")
 files.sort()
-
+model_name = config_clf['model_name']
 clf_results = pd.DataFrame(columns=["test_sub_id", 
                                     "classifier", 
                                     "accuracy_train", 
@@ -44,13 +47,13 @@ for file in files:
 
     clf = AAD_Classifier(model_name=config_clf['model_name'])
     optimization_results = clf.optimize_hyperparmeters(train_dataset, n_splits=10)
-    optimization_results_filename = f"{config_id}_reports-clf-optimization_models-{config_clf['model_name']}_sub-{test_sub_id}"
+    optimization_results_filename = f"dataset-{dataset_name}_reports-clf-optimization_models-{model_name}_config-{config_id_clf}_sub-{test_sub_id}"
     optimization_results.to_csv(os.path.join(path_dict['reports'], f"{optimization_results_filename}.csv"))
 
     clf.train(train_dataset)
     print(f"Accuracy Train: {clf.eval(train_dataset):.3f}")
     print(f"Accuracy Test: {clf.eval(test_dataset):.3f}")
-    model_filename = f"{config_id}_models-{config_clf['model_name']}_sub-{test_sub_id}"
+    model_filename = f"dataset-{dataset_name}_models-{model_name}_config-{config_id_clf}_sub-{test_sub_id}"
     clf.save(os.path.join(path_dict['models'], f"{model_filename}.pkl"))
 
     results_dict = {"test_sub_id": test_sub_id, 
@@ -63,7 +66,7 @@ for file in files:
                     }
     clf_results = pd.concat([clf_results, pd.DataFrame([results_dict])])
 
-clf_results_filename = f"{config_id}_reports-clf-performance_models-{config_clf['model_name']}"
+clf_results_filename = f"dataset-{dataset_name}_reports-clf-performance_models-{model_name}_config-{config_id_clf}"
 clf_results.to_csv(os.path.join(path_dict['reports'], f"{clf_results_filename}.csv"))
 
 # clf_results = pd.read_csv(os.path.join(results_path, f"classification_results_{config_clf['model_name']}.csv"))
