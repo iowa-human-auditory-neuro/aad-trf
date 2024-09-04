d3223c5fab8c8bdd5e735d50bf39645503ff9690
Jusung Ham <jusungham25@gmail.com>
2024-09-03 19:20:44 -0500

    feat: added `main()` method to the `TRF` class
    - The representational implementation of TRF class is integrated into the TRF class as a `main()` method.

--- src/aad_trf/trf.py
+++ src/aad_trf/trf.py
@@ -209,7 +209,89 @@ class TRF:
         normym = norm(ym, axis=axis)
 
         return np.sum(xm * ym, axis=axis) / (normxm * normym)
+    
+    def main(args=None):
+        import mne
+
+        from utils import load_config, set_paths_from_config
+
+        config_id = args.config_id
+
+        config = load_config(config_id)
+        config_id_audio = config['config_id_audio']
+        config_id_eeg = config['config_id_eeg']
+
+        dataset_name = config['dataset']
+        base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        path_dict = set_paths_from_config(base_path, config)
+
+        audio_filename = f"dataset-{dataset_name}_data-audio_config-{config_id_audio}"
+        audio = np.load(os.path.join(path_dict['features'], f"{audio_filename}.npy"))
+        eeg_filename = f"dataset-{dataset_name}_data-eeg_config-{config_id_eeg}_-epo"
+        epochs = mne.read_epochs(os.path.join(path_dict['features'], f"{eeg_filename}.fif"))
+        dataset = AAD_Dataset()
+        dataset.create(epochs, audio)
+        dataset.normalize() if config['normalize'] else None
+        del audio, epochs
+
+        cv = LeaveOneGroupOut()
+        groups = dataset.sub_ids
+        search_space = np.logspace(config['search_space'][0], 
+                                config['search_space'][1], 
+                                config['search_space'][2]
+                                )
+        eeg_prediction = []
+        for train_index, test_index in cv.split(dataset.labels, groups=groups):
+            train_dataset = dataset[train_index]
+            test_dataset = dataset[test_index]
+            test_sub_id = test_dataset.sub_ids[0]
+            print(f"Test subject: {test_sub_id}")
+
+            trf = TRF(direction=config['direction'], 
+                    delays=tuple(config['delays']), 
+                    scoring=config['scoring']
+                    )
+            trf.optimize_hyperparmeters(train_dataset, 
+                                        search_space, 
+                                        n_folds=config['n_folds']
+                                        )
+            trf.train(train_dataset)
+            # trf_filename = f"dataset-{dataset_name}_models-trf_config-{config_id}_sub-{test_sub_id}"
+            # trf.save(os.path.join(path_dict['models'], f"{trf_filename}.pkl"))
+            eeg_prediction.append(trf.predict(test_dataset))
+
+            dataset = trf.parse_scores_as_features(dataset)
+            # dataset_filename = f"dataset-{dataset_name}_data-aad-trfscores_config-{config_id}_sub-{test_sub_id}"
+            # dataset.save(os.path.join(path_dict['features'], f"{dataset_filename}.pkl"))
+
+            for type in ['coef', 'scores', 'hp-tuning', 'prediction']:
+                print(f"Plotting {type}...")
+                plot_channels = ['FCz']
+                plot_channels_str = "".join(plot_channels)
+                fig_filename = f"dataset-{dataset_name}_reports-trf-{type}_config-{config_id}_sub-{test_sub_id}_ch-{plot_channels_str}"
+                ax = trf.plot(type=type, 
+                                dataset=test_dataset,
+                                plot_channels=plot_channels,
+                                save=False,
+                                save_path=os.path.join(path_dict['reports'], f"{fig_filename}.png")
+                                )
+
+        # Grand Average EEG - TRF prediction
+        eeg_prediction = np.concatenate(eeg_prediction, axis=1)
+        # fig_filename = f"dataset-{dataset_name}_reports-trf-prediction_config-{config_id}_sub-grand-average_ch-{plot_channels_str}"
+        # save_path = os.path.join(path_dict['reports'], f"{fig_filename}.png")
+        axs = plot_eeg_prediction(dataset,
+                                eeg_prediction,
+                                plot_channels=plot_channels,
+                                scaling_factor=1.5
+                                )
+        # plt.savefig(save_path)
 
 if __name__ == '__main__':
-    pass
-        
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_id", type=str, default="trf-001", help="Configuration ID for TRF")
+    args = parser.parse_args()
+
+    TRF.main(args)
