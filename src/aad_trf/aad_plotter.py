1eeadc1eb8b781d2883361c2601046985e88208a
Jusung Ham <jusungham25@gmail.com>
2024-09-20 17:20:19 -0500

    feat: calculate and plot feature importance
    - new method `aad_classifier.get_feature_importance()` will retrieve importance values from trained classifier
    - new method `plot_importance()` will plot topography of feature importances

--- src/aad_trf/aad_plotter.py
+++ src/aad_trf/aad_plotter.py
@@ -201,5 +201,45 @@ def plot_clf_results(clf_results:pd.DataFrame):
 
     return ax
 
+def plot_importance(importance:np.ndarray,
+                    eeg_info:mne.Info, 
+                    ):
+    importance_half_idx = len(importance) // 2
+    importance_up = importance[:importance_half_idx]
+    importance_down = importance[importance_half_idx:]
+    vlim = (-np.abs(importance).max(), np.abs(importance).max())
+
+    if set(['FPz', 'Oz', 'T7', 'T8']) <= set(eeg_info['ch_names']):
+        sphere = 'eeglab'
+    else:
+        sphere = 'auto'
+
+    fig, axs = plt.subplots(1,2,figsize=(6,4))
+    score_topography, _ = plot_topomap(importance_up, 
+                                    pos=eeg_info,
+                                    show=False,
+                                    vlim=vlim,
+                                    sphere=sphere,
+                                    axes=axs[0]
+                                    )
+    axs[0].set_title("Up")
+    score_topography, _ = plot_topomap(importance_down, 
+                                    pos=eeg_info,
+                                    show=False,
+                                    vlim=vlim,
+                                    sphere=sphere,
+                                    axes=axs[1]
+                                    )
+    axs[1].set_title("Down")
+    fig.colorbar(score_topography, 
+                 orientation='vertical', 
+                 shrink=0.5,
+                 ticks=[vlim[0], 0, vlim[1]],
+                 ax=axs
+                 )
+    fig.suptitle("Feature importance across channels")
+
+    return axs
+
 if __name__ == '__main__':
     pass
